5fb462d0185f2847497fb244c3d5e27c7a37246a
Osaite Arigbe-Osula <ik.arigbe.osula@gmail.com>
2023-11-24 15:15:51 -0800

    me vs. checker round 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-exec { 'install_flask':
-  command => 'pip3 install flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin:/bin',
-  unless  => 'pip3 show flask | grep -q "Version: 2.1.0"',
+package { ['flask==2.1.0', 'werkzeug==2.1.1']:
+  ensure   => 'present',
+  provider => 'pip3',
 }
