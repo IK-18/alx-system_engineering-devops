8c05c8147f25116d9a0e48864b61787e4891a896
IK-18 <ik.arigbe.osula@gmail.com>
2024-02-13 16:26:14 -0800

    style corrections

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,42 +6,43 @@ import requests
 
 
 def count_words(subreddit, word_list, instances={}, after="", count=0):
-	"""
-	Prints a sorted count of given keywords
-	"""
-	url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-	headers = {
-		"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/effective-zone4090)"
-	}
-	params = {
-		"after": after,
-		"count": count,
-        "limit": 100
-    }
-	res = requests.get(url, headers=headers, params=params, allow_redirects=False)
-	try:
-		if res.status_code == 404:
-			raise Exception
-		posts = res.json().get("data")
-	except Exception:
-		print("")
-		return
-	after = posts.get("after")
-	count += posts.get("dist")
-	for post in posts.get("children"):
-		title = post.get("data").get("title").lower().split()
-		for word in word_list:
-			if word in word_list:
-				word_count = len([t for t in title if t == word.lower()])
-				if instances.get(word) is None:
-					instances[word] = word_count
-				else:
-					instances[word] += word_count
-	if after is None:
-		if len(instances) == 0:
-			print("")
-			return
-		instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-		[print(f"{key}: {value}") for key, value in instances]
-	else:
-		count_words(subreddit, word_list, instances, after, count)
+    """
+    Prints a sorted count of given keywords
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0"
+            }
+    params = {
+            "after": after,
+            "count": count,
+            "limit": 100
+            }
+    res = requests.get(url, headers=headers, params=params,
+                       allow_redirects=False)
+    try:
+        if res.status_code == 404:
+            raise Exception
+        posts = res.json().get("data")
+    except Exception:
+        print("")
+        return
+    after = posts.get("after")
+    count += posts.get("dist")
+    for post in posts.get("children"):
+        title = post.get("data").get("title").lower().split()
+        for word in word_list:
+            if word in word_list:
+                word_count = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = word_count
+                else:
+                    instances[word] += word_count
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print(f"{key}: {value}") for key, value in instances]
+    else:
+        count_words(subreddit, word_list, instances, after, count)
